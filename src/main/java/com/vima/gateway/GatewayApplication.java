dbc8908599794f8fd07a62573f144b79f1af9206
Vuk Milanovic <vuk.milanovic11@gmial.com>
2023-04-07 14:49:47 +0200

    init commit

---
+++ src/main/java/com/vima/gateway/GatewayApplication.java
@@ -0,0 +1,29 @@
+package com.vima.gateway;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator gateway(final RouteLocatorBuilder locatorBuilder) {
+		return locatorBuilder.routes()
+			.route("accommodation-ms", r -> r.path("/accommodation/**").uri("lb://accommodation-ms"))
+			.route("auth-ms", r -> r.path("/auth/**").uri("lb://auth-ms"))
+			.build();
+	}
+
+}
