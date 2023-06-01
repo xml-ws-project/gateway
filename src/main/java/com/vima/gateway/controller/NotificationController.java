f0318326e5296003ea75a6c11ffa7875a3d786e2
Vuk Milanovic <vuk.milanovic11@gmial.com>
2023-06-01 19:16:24 +0200

    Work in progress

---
+++ src/main/java/com/vima/gateway/controller/NotificationController.java
@@ -0,0 +1,42 @@
+package com.vima.gateway.controller;
+
+import com.vima.gateway.AccommodationServiceGrpc;
+import com.vima.gateway.dto.grpcObjects.gRPCObjectAccom;
+import com.vima.gateway.dto.grpcObjects.gRPCObjectUser;
+import com.vima.gateway.dto.notification.EditNotificationHttpRequest;
+import com.vima.gateway.mapper.notification.NotificationMapper;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/notification")
+@RequiredArgsConstructor
+public class NotificationController {
+
+	@PatchMapping("/")
+	public ResponseEntity<?> editNotificationOptions(@RequestBody @Valid EditNotificationHttpRequest request) {
+		var userBlockingStub = getBlockingStub();
+		var response = userBlockingStub.getStub().editNotificationOptions(NotificationMapper.convertHttpToGrpcEditRequest(request));
+		return ResponseEntity.ok(NotificationMapper.convertGrpcToHttpResponse(response));
+	}
+
+	private gRPCObjectUser getBlockingStub() {
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9092)
+			.usePlaintext()
+			.build();
+		return gRPCObjectUser.builder()
+			.channel(channel)
+			.stub(communication.userDetailsServiceGrpc.newBlockingStub(channel))
+			.build();
+	}
+}
