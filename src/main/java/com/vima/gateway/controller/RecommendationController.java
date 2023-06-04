b73d21b01bfc816440f97ad28496bbec4cea6f46
Andrija Stanišić <andrija.d.stanisic@gmail.com>
2023-06-05 01:37:47 +0200

    Recommendation ms/query (#10)
    * added functions for creating user and accom nod
    
    * changed port for recommendation service
    
    ---------
    
    Co-authored-by: Andrija25 <93194317+Andrija25@users.noreply.github.com>

---
+++ src/main/java/com/vima/gateway/controller/RecommendationController.java
@@ -0,0 +1,40 @@
+package com.vima.gateway.controller;
+
+import com.vima.gateway.RecommendationServiceGrpc;
+import com.vima.gateway.Uuid;
+import com.vima.gateway.dto.grpcObjects.gRPCObjectRec;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/recommendation")
+@RequiredArgsConstructor
+public class RecommendationController {
+
+    @PostMapping("/create/user/{id}")
+    public ResponseEntity<String> createUserNod(@PathVariable("id") final String userId){
+        var response = getBlockingStub().getStub().createUserNod(Uuid.newBuilder().setValue(userId).build());
+        getBlockingStub().getChannel().shutdown();
+        return ResponseEntity.ok(response.getValue());
+    }
+
+    @PostMapping("/create/accom/{id}")
+    public ResponseEntity<String> createAccomNod(@PathVariable("id") final String accomId){
+        var response = getBlockingStub().getStub().createAccomNod(Uuid.newBuilder().setValue(accomId).build());
+        getBlockingStub().getChannel().shutdown();
+        return ResponseEntity.ok(response.getValue());
+    }
+
+    private gRPCObjectRec getBlockingStub() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9095)
+                .usePlaintext()
+                .build();
+        return gRPCObjectRec.builder()
+                .channel(channel)
+                .stub(RecommendationServiceGrpc.newBlockingStub(channel))
+                .build();
+    }
+}
